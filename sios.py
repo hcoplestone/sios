f83951b1ba33aa29eb89d0d78eb1f08be849dcef
Harvey Coplestone <harveycoplestone@me.com>
2018-11-12 17:11:11 +0000

    added in old ggl file for reference

--- sios.py
+++ sios.py
@@ -4,8 +4,7 @@ from integrators import GalerkinGaussLobattoIntegrator
 class Sios:
     def doit(self):
         ggl = GalerkinGaussLobattoIntegrator('x', ['q1', 'q2'], ['v1', 'v2'], True)
-        ggl.discretise('x^2', 4, -1.0, 1.0)
-
+        ggl.discretise('x^2', 4, 1.0, 2.0)
 
 if __name__ == "__main__":
    sios = Sios()
