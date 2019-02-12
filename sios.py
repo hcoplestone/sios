fb961cb43cd0c0ea1858e675d450918b09d3ed1f
Harvey Coplestone <harveycoplestone@me.com>
2019-02-12 18:26:33 +0000

    timing

--- sios.py
+++ sios.py
@@ -1,9 +1,42 @@
 from integrators import FirstOrderIntegrator
 from timeit import default_timer as timer
+import matplotlib.pyplot as plt
 
 
 class Sios:
-    def timeit(self):
+    def timeit(self, n=200):
+        # Create an instance of our integrator
+        foi = FirstOrderIntegrator('t', ['x', 'y'], ['vx', 'vy'], verbose=True)
+
+        # Define our properties and the Lagrangian for a spring
+        m = 1.0
+
+        x, y = foi.symbols['q']
+        vx, vy = foi.symbols['v']
+
+        L = 0.5 * m * (vx * vx + vy * vy) - 2 * (x * x + y * y)
+
+        # Define discretization parameters
+        foi.discretise(L, n, 0.0, 10.0)
+
+        # Set the initial conditions for integration
+        foi.set_initial_conditions([1.0, 1.0], [0.0, 0.0])
+
+        # Integrate the system
+
+        start_time = timer()
+        foi.integrate()
+        end_time = timer()
+
+        # Plot the results
+        # foi.plot_results()
+
+        # Display elapsed time while integrating
+        elapsed_time = end_time - start_time
+        print('Elapsed time is {0:.2f}'.format(elapsed_time))
+        return elapsed_time
+
+    def doit(self):
         # Create an instance of our integrator
         foi = FirstOrderIntegrator('t', ['x', 'y'], ['vx', 'vy'], verbose=True)
 
@@ -28,7 +61,7 @@ class Sios:
         end_time = timer()
 
         # Plot the results
-        # foi.plot_results()
+        foi.plot_results()
 
         # Display elapsed time while integrating
         elapsed_time = end_time - start_time
@@ -38,7 +71,10 @@ class Sios:
 
 if __name__ == "__main__":
     sios = Sios()
-    times = []
-    for i in range(5):
-        times.append(sios.timeit())
-    print(times)
+    sios.doit()
+    # n = range(1,5)
+    # times = []
+    # for i in n:
+    #     times.append(sios.timeit(i))
+    # plt.plot(n, times)
+    # plt.show()
