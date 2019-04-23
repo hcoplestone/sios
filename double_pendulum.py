07c03de05a925abce428cdd201ab96a979ff81c1
Harvey Coplestone <harveycoplestone@me.com>
2019-04-23 22:37:51 +0100

    comparisons

--- double_pendulum.py
+++ double_pendulum.py
@@ -1,21 +1,21 @@
-from integrators import GalerkinGaussLobattoIntegrator
+from integrators import GalerkinGaussLobattoIntegrator, FirstOrderIntegrator
 from timeit import default_timer as timer
 import numpy as np
 
 import sympy.functions.elementary.trigonometric as sp_trig
 
 # Define our properties
-m1 = 1000.0
-m2 = 1.0
+m1 = 1.0
+m2 = 5.0
 
 l1 = 1.0
 l2 = 1.0
 
-theta_1_0 = 0.0
-theta_2_0 = np.pi/2
+theta_1_0 = 7 * np.pi / 8
+theta_2_0 = 0.0
 
-v_theta_1_0 = 0.0
-v_theta_2_0 = 0.0
+v_theta_1_0 = 0.5
+v_theta_2_0 = 0.2
 
 p_theta_1_0 = float(
     (m1 + m2) * (l1 ** 2) * v_theta_1_0 + m2 * l1 * l2 * v_theta_2_0 * np.cos(theta_2_0 - theta_1_0)
@@ -33,6 +33,8 @@ class DoublePendulum:
         # Create an instance of our integrator
         integrator = GalerkinGaussLobattoIntegrator('t', ['theta_1', 'theta_2'], ['v_theta_1', 'v_theta_2'], order,
                                                     verbose=True)
+        # integrator = FirstOrderIntegrator('t', ['theta_1', 'theta_2'], ['v_theta_1', 'v_theta_2'],
+        #                                            verbose=True)
 
         # Get symbols for use in Lagrangian
         theta_1, theta_2 = integrator.symbols['q']
@@ -40,13 +42,13 @@ class DoublePendulum:
 
         # Define the Lagrangian for the system
         L = 0.5 * (m1 + m2) * l1 * l1 * v_theta_1 * v_theta_1 \
-        + 0.5 * m2 * l2 * l2 * v_theta_2 * v_theta_2 \
-        + m2 * l1 * l2 * v_theta_1 * v_theta_2 * sp_trig.cos(theta_1 - theta_2) \
-        + (m1 + m2) * g * l1 * sp_trig.cos(theta_1) \
-        + m2 * g * l2 * sp_trig.cos(theta_2)
+            + 0.5 * m2 * l2 * l2 * v_theta_2 * v_theta_2 \
+            + m2 * l1 * l2 * v_theta_1 * v_theta_2 * sp_trig.cos(theta_1 - theta_2) \
+            + (m1 + m2) * g * l1 * sp_trig.cos(theta_1) \
+            + m2 * g * l2 * sp_trig.cos(theta_2)
 
         # Define discretization parameters
-        integrator.discretise(L, 100, 0.0, 1.0)
+        integrator.discretise(L, 2000, 0.0, 5.0)
 
         # Set the initial conditions for integration
         integrator.set_initial_conditions([theta_1_0, theta_2_0], [p_theta_1_0, p_theta_2_0])
@@ -65,7 +67,8 @@ class DoublePendulum:
 
 if __name__ == "__main__":
     sios = DoublePendulum()
-    doublependulum = sios.doit(2)
+    doublependulum = sios.doit(4)
     doublependulum.plot_results()
-    np.savez('data/double_pendulum.npz', q_list=doublependulum.q_list, t_list=doublependulum.t_list,
+    np.savez('data/double_pendulum_order_comparison_4_limit.npz', ms=np.array([m1, m2]), ls=np.array([l1, l2]),
+             q_list=doublependulum.q_list, t_list=doublependulum.t_list,
              q_solutions=doublependulum.q_solutions, p_solutions=doublependulum.p_solutions)
