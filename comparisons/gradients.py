1cbd35f069dd5a92204d8ead331f8cc618434abc
Harvey Coplestone <harveycoplestone@me.com>
2019-05-02 12:35:24 +0100

    comparisons

--- comparisons/gradients.py
+++ comparisons/gradients.py
@@ -6,7 +6,7 @@ from autograd import grad
 
 
 def f_autograd(y):
-    return y * y * ag_np.exp(y) + 2 * y * ag_np.sin(y)
+    return (y * y * ag_np.exp(y) + 2 * y * ag_np.sin(y))**3
 
 
 xval = 2.5
@@ -16,7 +16,7 @@ autograd_eval = autograd_f(xval)
 print('Autograd: f({}) = {}'.format(xval, autograd_eval))
 
 x = sp.Symbol('x')
-f_sympy = x * x * sp.exp(x) + 2 * x * sp.sin(x)
+f_sympy = (x * x * sp.exp(x) + 2 * x * sp.sin(x))**3
 
 symbolgrad_f = sp.diff(f_sympy, x)
 symbol_eval = symbolgrad_f.evalf(16, subs={x: xval})
